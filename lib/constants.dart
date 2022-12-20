f00ae539b6e72c14f02f575640aafab3ddbc402c
Motdep@sse <lirsitogo2021@gmail.com>
2022-12-20 17:01:58 +0000

    new file:   lib/constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const white = Colors.white;
+const uiBgBlack = Color(0xFF03123D);
+
+//Font
+final poppins = GoogleFonts.poppins();
